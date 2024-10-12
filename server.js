3893597b7ebabd267e15b884f7e92f798e2ea2c0
ulfhiraa <studyiraa@gmail.com>
2024-10-12 12:17:55 +0700

    node install, express install and setting port

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.listen(PORT, () => 
+    console.log(
+        `Server berjalan di port : http://localhost:${PORT}`)
+);
